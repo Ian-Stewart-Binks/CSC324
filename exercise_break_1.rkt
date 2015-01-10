f583f3c1d6681958ddde5732c971724579f48f9a
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-01-09 22:28:46 -0500

    EB1: Add third exercise.

--- exercise_break_1.rkt
+++ exercise_break_1.rkt
@@ -4,3 +4,6 @@
 
 ; Exercise 2: Write a function to determine if a given element appears in a list.
 (define (appears lst item) (> (length (filter (lambda (x) (equal? x item)) lst)) 0))
+
+; Exercise 3: Write a function to determine the number of duplicates in a list.
+(define (num_duplicates lst) (length (filter (lambda (x) (member x lst)) lst)))
