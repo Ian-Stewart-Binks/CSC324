ff1d030513a4977166e86e94faa347622c641d11
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-01-10 00:49:27 -0500

    EB1: Add fifth exercise, clean up 3 and 4.

--- exercise_break_1.rkt
+++ exercise_break_1.rkt
@@ -6,7 +6,14 @@
 (define (appears lst item) (> (length (filter (lambda (x) (equal? x item)) lst)) 0))
 
 ; Exercise 3: Write a function to determine the number of duplicates in a list.
-(define (num_duplicates lst) (length (filter (lambda (x) (member x (remove x lst))) lst)))
+(define (num_duplicates lst) (- (length lst) (length (remove-duplicates lst))))
 
 ; Exercise 4: Write a function to remove all duplicates from a list.
-(define (remove_duplicates lst) (filter (lambda (x) (not (member x (remove x lst)))) lst))
+(define (remove_duplicates lst) (foldl (lambda (x y) (if (member x (remove x y)) (remove x y) y)) lst lst))
+
+; Exercise 5 a: Given two lists, output the items that appear in both lists (intersection).
+(define (intersection lst1 lst2) (let ([lst3 (filter (lambda (x) (member x lst2)) lst1)])
+                                   (append lst3 (filter (lambda (x) (and (member x lst1) (not (member x lst3)))) lst2))))
+
+; Exercise 5 b: Then, output the items that appear in at least one of the two lists.
+(define (union lst1 lst2) (remove-duplicates (append lst1 lst2)))
