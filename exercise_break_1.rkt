4053f59461078af913659a80c89e0e7bf0b7c089
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-01-09 22:03:47 -0500

    EB1: Add second exercise.

--- exercise_break_1.rkt
+++ exercise_break_1.rkt
@@ -1,3 +1,6 @@
 #lang racket
-; Exercise 1
+; Exercise 1: Write a function to determine the length of a list.
 (define (len lst) (if (empty? lst) 0 (+ 1 (len (list-tail lst 1)))))
+
+; Exercise 2: Write a function to determine if a given element appears in a list.
+(define (appears lst item) (> (length (filter (lambda (x) (equal? x item)) lst)) 0))
